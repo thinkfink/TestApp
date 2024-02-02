fe141fc69f74963431e074000c8722631dfaed58
Tyler Fink <tylerfink95@gmail.com>
2024-02-02 12:58:28 -0600

    Added list and loops
    Added list and loops

--- testapp.py
+++ testapp.py
@@ -23,6 +23,35 @@ print(message[:5])
 print(message[6:])
 print(message.lower())
 print(message.count('l'))
-print(message.find('World'))]
+print(message.find('World'))
+
+###Lists
+todo_list = [
+    "buy groceries",
+    "do the dishes",
+    "mow the lawn"
+]
+
+#for loop
+for thing in todo_list:
+    print(thing.title())
+
+#loop over thing and index
+for i in range(len(todo_list)):
+    print(i, todo_list[i].title())
+
+#counter
+counter = 0
+for thing in todo_list:
+    print(counter, thing.title())
+    counter += 1
+
+#enumerate
+    for i, thing in enumerate(todo_list):
+        print(i, thing.title())
 
 ###Numbers
+
+
+
+
