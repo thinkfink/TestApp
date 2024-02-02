f2ce135fef234ca64816043695550cba8e17d7de
Tyler Fink <tylerfink95@gmail.com>
2024-02-01 20:44:22 -0600

    Working with strings
    Printing and formatting strings, string methods

---
+++ testapp.py
@@ -0,0 +1,28 @@
+###Strings
+
+#message = 'Hello World!'
+
+greeting = 'Hello'
+name = 'Tyler'
+
+#print(dir(name))
+
+print(help(str.lower))
+
+#message = greeting + ', ' + name + '. Welcome!'
+
+message = '{}, {}. Welcome!'.format(greeting, name)
+
+message = f'{greeting}, {name.upper}. Welcome!'
+
+#message = message.replace('World','Universe')
+
+print(message)
+#print(message[0:5])'Hello World!'
+print(message[:5])
+print(message[6:])
+print(message.lower())
+print(message.count('l'))
+print(message.find('World'))]
+
+###Numbers
